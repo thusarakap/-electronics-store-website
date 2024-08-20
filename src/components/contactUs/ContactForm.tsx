80815a2c74d5a9b6cccf6078d2b9e1790d27b7c0
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-08-20 17:20:18 +0530

    Update ContactForm.tsx

--- src/components/contactUs/ContactForm.tsx
+++ src/components/contactUs/ContactForm.tsx
@@ -5,39 +5,38 @@ import { Textarea } from "@/components/ui/textarea"
 
 export function ContactForm() {
   return (
-    <section className="w-full bg-muted py-12 md:py-20 px-6 md:px-10">
-          <div className="max-w-6xl mx-auto">
-            <h2 className="text-2xl md:text-3xl font-bold mb-6">Registration Form</h2>
+    <div className="flex justify-center">
+    <section className="bg-muted py-8 px-8 rounded-xl container mx-auto max-w-6xl">
+          <div>
+            <h2 className="text-2xl md:text-3xl font-bold mb-6">Contact Form</h2>
             <form className="grid gap-4">
               <div className="grid md:grid-cols-2 gap-4">
                 <div className="space-y-2">
-                  <Label htmlFor="name">Full Name</Label>
-                  <Input id="name" placeholder="Enter your full name" />
+                  <Label htmlFor="name">Name</Label>
+                  <Input id="name" placeholder="Enter your name" />
                 </div>
                 <div className="space-y-2">
                   <Label htmlFor="email">Email</Label>
                   <Input id="email" type="email" placeholder="Enter your email" />
                 </div>
               </div>
-              <div className="grid md:grid-cols-2 gap-4">
+              <div className="w-3/4">
                 <div className="space-y-2">
-                  <Label htmlFor="phone">Phone Number</Label>
-                  <Input id="phone" type="tel" placeholder="Enter your phone number" />
+                  <Label htmlFor="phone">Subject</Label>
+                  <Input id="subject" type="text" placeholder="Enter the subject" />
                 </div>
-                <div className="space-y-2">
-                  <Label htmlFor="school">Previous School</Label>
-                  <Input id="school" placeholder="Enter your previous school" />
-                </div>
+                
               </div>
               <div className="space-y-2">
                 <Label htmlFor="message">Message</Label>
                 <Textarea id="message" placeholder="Write your message" rows={4} />
               </div>
               <Button type="submit" className="justify-center">
-                Register Now
+                Send
               </Button>
             </form>
           </div>
         </section>
+      </div>
   );
 }   
